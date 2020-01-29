0f7f90becba2c863df5a857e4dd0a670915cb67c
Diceroll21 <59959777+Diceroll21@users.noreply.github.com>
2020-01-29 14:24:51 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::any('exercise-1', 'Exercise1');
+Route::any('DignadiceEx1', 'Exercise1');
 Route::any('DignadiceEx2', 'Exercise2');
 Route::any('DignadiceEx3', 'Exercise3@index');
-Route::any('exercise-3/store', 'Exercise3@store');
+Route::any('Dignadice/store', 'Exercise3@store');
 Route::any('Dignadice_final', 'ExerciseFinal@login');
 Route::any('Dignadice_final/store', 'ExerciseFinal@store');
